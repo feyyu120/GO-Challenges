445cd0dedbcf3e779df7988b40065fc12df42c7f
feyyu120 <feyselfeyyu@gmail.com>
2026-08-04 09:12:14 +0300

    solved

--- missing-number.go
+++ missing-number.go
@@ -1,1 +1,19 @@
-package gochallenges
+package main
+
+import "fmt"
+
+func main() {
+	var n int
+	fmt.Println("enter size the integers")
+	fmt.Scan(&n)
+	var missed int
+	for i := 1; i < n; i++ {
+		var num int
+		fmt.Scan(&num)
+		if i != num {
+			missed = i
+		}
+	}
+	fmt.Println(missed)
+
+}
