80676bd28be75cf1de8159248d03c57a21daff5d
feyyu120 <feyselfeyyu@gmail.com>
2026-08-07 16:50:30 +0300

    solved

--- maximum-subarray.go
+++ maximum-subarray.go
@@ -5,16 +5,37 @@ import "fmt"
 func main() {
 	var n int
 	fmt.Scan(&n)
-	var nums []int
-	for i := 0; i < n; i++ {
-		var num int
-		fmt.Scan(&num)
-		nums = append(nums, num)
+
+	// Handle empty input edge case
+	if n <= 0 {
+		fmt.Println(0)
+		return
 	}
-	maxVal := nums[0] + nums[1]
+
+	nums := make([]int, n)
 	for i := 0; i < n; i++ {
-		for j := 1; j < n; j++ {
-			nums[i] + nums[j]
+		fmt.Scan(&nums[i])
+	}
+
+	// Initialize Kadane's Algorithm variables
+	maxSoFar := nums[0]
+	currentMax := nums[0]
+
+	// Loop through the array starting from the second element
+	for i := 1; i < n; i++ {
+		// Decide whether to add the current number to the existing subarray
+		// or start a brand new subarray from the current number
+		if nums[i] > currentMax+nums[i] {
+			currentMax = nums[i]
+		} else {
+			currentMax = currentMax + nums[i]
+		}
+
+		// Update the overall maximum found so far
+		if currentMax > maxSoFar {
+			maxSoFar = currentMax
 		}
 	}
+
+	fmt.Println(maxSoFar)
 }
