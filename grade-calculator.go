32588821d6fa70656ad5a08ec1dc4f5749e0dd03
feyyu120 <feyselfeyyu@gmail.com>
2026-08-11 20:46:20 +0300

    ready to use another functions

--- grade-calculator.go
+++ grade-calculator.go
@@ -2,24 +2,29 @@ package main
 
 import "fmt"
 
-type courses struct {
+type Courses struct {
 	Name        string
+	Point       float64
 	Credit_hour int
 }
 
 func main() {
+	var course []Courses
 	var n int
-	course := make([]courses, 0, n)
 	fmt.Println("enter the the length of the course you take")
 	fmt.Scan(&n)
 	fmt.Println("Enter name and credit hour of the course")
 	for i := 0; i < n; i++ {
 		var name string
+		var point float64
 		var credit_hour int
-		fmt.Scan(&name, &credit_hour)
-		course[i].Name = name
-		course[i].Credit_hour = credit_hour
+		fmt.Scan(&name, &point, &credit_hour)
+		course = append(course, Courses{Name: name, Credit_hour: credit_hour})
 	}
 
-	fmt.Println(course)
+	fmt.Println(course[0].Credit_hour)
+}
+
+func Calculate(name string, credit, n int) {
+
 }
