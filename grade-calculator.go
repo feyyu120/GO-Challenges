aaedf8dca76cb013ffba908e9f63e13df865f63c
feyyu120 <feyselfeyyu@gmail.com>
2026-08-11 21:11:20 +0300

    solved as simple

--- grade-calculator.go
+++ grade-calculator.go
@@ -10,21 +10,35 @@ type Courses struct {
 
 func main() {
 	var course []Courses
+	var total int = 0
 	var n int
 	fmt.Println("enter the the length of the course you take")
 	fmt.Scan(&n)
-	fmt.Println("Enter name and credit hour of the course")
+	fmt.Println("Enter name,point and credit hour of the course")
 	for i := 0; i < n; i++ {
 		var name string
 		var point float64
 		var credit_hour int
 		fmt.Scan(&name, &point, &credit_hour)
-		course = append(course, Courses{Name: name, Credit_hour: credit_hour})
+		total += credit_hour
+		course = append(course, Courses{Name: name, Point: point, Credit_hour: credit_hour})
 	}
+	Calculate(course, n, total)
 
-	fmt.Println(course[0].Credit_hour)
 }
 
-func Calculate(name string, credit, n int) {
+func Calculate(course []Courses, n int, total int) {
+	result := 0
+	for i := 0; i < n; i++ {
+		c := course[i].Credit_hour
+		val := course[i].Point
+		switch {
+		case val > 85:
+			result += 4 * c
+		case val > 70:
+			result += 3 * c
+		}
+	}
 
+	fmt.Println("Your grade is", result/total)
 }
